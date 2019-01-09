bf2047d5cd01c5638e4800e09fc74fcba059220b
Antoniu Miclaus <antoniu.miclaus@analog.com>
2019-01-09 12:03:48 +0200

    travis: download and run common scripts
    Add 'lib.sh' script that downloads the travis required scripts from the main
    repo that holds them (no-OS).
    
    Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

--- ci/travis/run_build.sh
+++ ci/travis/run_build.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 set -e
 
+. ./ci/travis/lib.sh
+
 build_default() {
-    . ./ci/scripts/cppcheck.sh
+    export CPPCHECK_OPTIONS="-i ./projects/ADuCM3029_ArrowConnect_Greenhouse/src"
+    . ./build/cppcheck.sh 
 }
 
 build_astyle() {
-    . ./ci/scripts/astyle.sh
+    . ./build/astyle.sh
 }
 
 build_${BUILD_TYPE:-default}
