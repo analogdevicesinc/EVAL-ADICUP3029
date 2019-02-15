9b8d3443afb8b039589972d7cd085a8dfa94ba18
Andrei Drimbarean <Andrei.Drimbarean@analog.com>
2019-02-15 10:12:44 +0200

    cli_demo: fix compiler path to "include" folder
    The compiler path to the "include" folder was a literal path to the
    folder three on my specific machine and not a variable path to the
    workspace folder. As such, the project could not be compiled on other
    machines. Update the compiler path to the "include" folder to resolve
    this issue.
    
    Signed-off-by: Andrei Drimbarean <Andrei.Drimbarean@analog.com>

--- projects/ADuCM3029_demo_cli/Debug/RTE/Device/ADuCM3029/subdir.mk
+++ projects/ADuCM3029_demo_cli/Debug/RTE/Device/ADuCM3029/subdir.mk
@@ -4,12 +4,12 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.7.0/ARM/packs/AnalogDevices/ADuCM302x_DFP/3.1.0/Source/drivers/dma/adi_dma.c \
-C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.7.0/ARM/packs/AnalogDevices/ADuCM302x_DFP/3.1.0/Source/drivers/pwr/adi_pwr.c \
-C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.7.0/ARM/packs/AnalogDevices/ADuCM302x_DFP/3.1.0/Source/drivers/tmr/adi_tmr.c \
-C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.7.0/ARM/packs/AnalogDevices/ADuCM302x_DFP/3.1.0/Source/drivers/uart/adi_uart.c \
-C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.7.0/ARM/packs/AnalogDevices/ADuCM302x_DFP/3.1.0/Source/drivers/wdt/adi_wdt.c \
-C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.7.0/ARM/packs/AnalogDevices/ADuCM302x_DFP/3.1.0/Source/common.c \
+C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.8.0/ARM/packs/AnalogDevices/ADuCM302x_DFP/3.2.0/Source/drivers/dma/adi_dma.c \
+C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.8.0/ARM/packs/AnalogDevices/ADuCM302x_DFP/3.2.0/Source/drivers/pwr/adi_pwr.c \
+C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.8.0/ARM/packs/AnalogDevices/ADuCM302x_DFP/3.2.0/Source/drivers/tmr/adi_tmr.c \
+C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.8.0/ARM/packs/AnalogDevices/ADuCM302x_DFP/3.2.0/Source/drivers/uart/adi_uart.c \
+C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.8.0/ARM/packs/AnalogDevices/ADuCM302x_DFP/3.2.0/Source/drivers/wdt/adi_wdt.c \
+C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.8.0/ARM/packs/AnalogDevices/ADuCM302x_DFP/3.2.0/Source/common.c \
 ../RTE/Device/ADuCM3029/startup_ADuCM3029.c \
 ../RTE/Device/ADuCM3029/system_ADuCM3029.c 
 
@@ -39,59 +39,59 @@ C_DEPS += \
 
 
 # Each subdirectory must supply rules for building sources it contributes
-RTE/Device/ADuCM3029/adi_dma.o: C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.7.0/ARM/packs/AnalogDevices/ADuCM302x_DFP/3.1.0/Source/drivers/dma/adi_dma.c
+RTE/Device/ADuCM3029/adi_dma.o: C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.8.0/ARM/packs/AnalogDevices/ADuCM302x_DFP/3.2.0/Source/drivers/dma/adi_dma.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: CrossCore GCC ARM Embedded C Compiler'
-	arm-none-eabi-gcc -g -gdwarf-2 -ffunction-sections -fdata-sections -DCORE0 -D_DEBUG -D_RTE_ -D__ADUCM3029__ -D__SILICON_REVISION__=0x100 @includes-ace88a7f676e681455857b8b214a14d7.txt -Wall -c -mcpu=cortex-m3 -mthumb -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o  "$@" "$<"
+	arm-none-eabi-gcc -g -gdwarf-2 -ffunction-sections -fdata-sections -DCORE0 -D_DEBUG -D_RTE_ -D__ADUCM3029__ -D__SILICON_REVISION__=0x100 @includes-afa06b5f1ce396f0c89a226ccea14b75.txt -Wall -c -mcpu=cortex-m3 -mthumb -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o  "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
-RTE/Device/ADuCM3029/adi_pwr.o: C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.7.0/ARM/packs/AnalogDevices/ADuCM302x_DFP/3.1.0/Source/drivers/pwr/adi_pwr.c
+RTE/Device/ADuCM3029/adi_pwr.o: C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.8.0/ARM/packs/AnalogDevices/ADuCM302x_DFP/3.2.0/Source/drivers/pwr/adi_pwr.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: CrossCore GCC ARM Embedded C Compiler'
-	arm-none-eabi-gcc -g -gdwarf-2 -ffunction-sections -fdata-sections -DCORE0 -D_DEBUG -D_RTE_ -D__ADUCM3029__ -D__SILICON_REVISION__=0x100 @includes-ace88a7f676e681455857b8b214a14d7.txt -Wall -c -mcpu=cortex-m3 -mthumb -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o  "$@" "$<"
+	arm-none-eabi-gcc -g -gdwarf-2 -ffunction-sections -fdata-sections -DCORE0 -D_DEBUG -D_RTE_ -D__ADUCM3029__ -D__SILICON_REVISION__=0x100 @includes-afa06b5f1ce396f0c89a226ccea14b75.txt -Wall -c -mcpu=cortex-m3 -mthumb -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o  "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
-RTE/Device/ADuCM3029/adi_tmr.o: C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.7.0/ARM/packs/AnalogDevices/ADuCM302x_DFP/3.1.0/Source/drivers/tmr/adi_tmr.c
+RTE/Device/ADuCM3029/adi_tmr.o: C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.8.0/ARM/packs/AnalogDevices/ADuCM302x_DFP/3.2.0/Source/drivers/tmr/adi_tmr.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: CrossCore GCC ARM Embedded C Compiler'
-	arm-none-eabi-gcc -g -gdwarf-2 -ffunction-sections -fdata-sections -DCORE0 -D_DEBUG -D_RTE_ -D__ADUCM3029__ -D__SILICON_REVISION__=0x100 @includes-ace88a7f676e681455857b8b214a14d7.txt -Wall -c -mcpu=cortex-m3 -mthumb -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o  "$@" "$<"
+	arm-none-eabi-gcc -g -gdwarf-2 -ffunction-sections -fdata-sections -DCORE0 -D_DEBUG -D_RTE_ -D__ADUCM3029__ -D__SILICON_REVISION__=0x100 @includes-afa06b5f1ce396f0c89a226ccea14b75.txt -Wall -c -mcpu=cortex-m3 -mthumb -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o  "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
-RTE/Device/ADuCM3029/adi_uart.o: C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.7.0/ARM/packs/AnalogDevices/ADuCM302x_DFP/3.1.0/Source/drivers/uart/adi_uart.c
+RTE/Device/ADuCM3029/adi_uart.o: C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.8.0/ARM/packs/AnalogDevices/ADuCM302x_DFP/3.2.0/Source/drivers/uart/adi_uart.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: CrossCore GCC ARM Embedded C Compiler'
-	arm-none-eabi-gcc -g -gdwarf-2 -ffunction-sections -fdata-sections -DCORE0 -D_DEBUG -D_RTE_ -D__ADUCM3029__ -D__SILICON_REVISION__=0x100 @includes-ace88a7f676e681455857b8b214a14d7.txt -Wall -c -mcpu=cortex-m3 -mthumb -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o  "$@" "$<"
+	arm-none-eabi-gcc -g -gdwarf-2 -ffunction-sections -fdata-sections -DCORE0 -D_DEBUG -D_RTE_ -D__ADUCM3029__ -D__SILICON_REVISION__=0x100 @includes-afa06b5f1ce396f0c89a226ccea14b75.txt -Wall -c -mcpu=cortex-m3 -mthumb -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o  "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
-RTE/Device/ADuCM3029/adi_wdt.o: C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.7.0/ARM/packs/AnalogDevices/ADuCM302x_DFP/3.1.0/Source/drivers/wdt/adi_wdt.c
+RTE/Device/ADuCM3029/adi_wdt.o: C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.8.0/ARM/packs/AnalogDevices/ADuCM302x_DFP/3.2.0/Source/drivers/wdt/adi_wdt.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: CrossCore GCC ARM Embedded C Compiler'
-	arm-none-eabi-gcc -g -gdwarf-2 -ffunction-sections -fdata-sections -DCORE0 -D_DEBUG -D_RTE_ -D__ADUCM3029__ -D__SILICON_REVISION__=0x100 @includes-ace88a7f676e681455857b8b214a14d7.txt -Wall -c -mcpu=cortex-m3 -mthumb -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o  "$@" "$<"
+	arm-none-eabi-gcc -g -gdwarf-2 -ffunction-sections -fdata-sections -DCORE0 -D_DEBUG -D_RTE_ -D__ADUCM3029__ -D__SILICON_REVISION__=0x100 @includes-afa06b5f1ce396f0c89a226ccea14b75.txt -Wall -c -mcpu=cortex-m3 -mthumb -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o  "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
-RTE/Device/ADuCM3029/common.o: C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.7.0/ARM/packs/AnalogDevices/ADuCM302x_DFP/3.1.0/Source/common.c
+RTE/Device/ADuCM3029/common.o: C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.8.0/ARM/packs/AnalogDevices/ADuCM302x_DFP/3.2.0/Source/common.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: CrossCore GCC ARM Embedded C Compiler'
-	arm-none-eabi-gcc -g -gdwarf-2 -ffunction-sections -fdata-sections -DCORE0 -D_DEBUG -D_RTE_ -D__ADUCM3029__ -D__SILICON_REVISION__=0x100 @includes-ace88a7f676e681455857b8b214a14d7.txt -Wall -c -mcpu=cortex-m3 -mthumb -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o  "$@" "$<"
+	arm-none-eabi-gcc -g -gdwarf-2 -ffunction-sections -fdata-sections -DCORE0 -D_DEBUG -D_RTE_ -D__ADUCM3029__ -D__SILICON_REVISION__=0x100 @includes-afa06b5f1ce396f0c89a226ccea14b75.txt -Wall -c -mcpu=cortex-m3 -mthumb -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o  "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
 RTE/Device/ADuCM3029/%.o: ../RTE/Device/ADuCM3029/%.S
 	@echo 'Building file: $<'
 	@echo 'Invoking: CrossCore GCC ARM Embedded Assembler'
-	arm-none-eabi-gcc -c -x assembler-with-cpp -g -gdwarf-2 -DCORE0 -D_DEBUG -D_RTE_ -D__ADUCM3029__ -D__SILICON_REVISION__=0x100 -I"C:\Users\ADrimbar\git\EVAL-ADICUP3029\projects\ADuCM3029_demo_cli\system" -I"C:/Analog Devices/CrossCore Embedded Studio 2.7.0/ARM/packs/ARM/CMSIS/5.2.0/CMSIS/Include" -I"C:/Analog Devices/CrossCore Embedded Studio 2.7.0/ARM/packs/AnalogDevices/ADuCM302x_DFP/3.1.0/Include" -I"C:/Analog Devices/CrossCore Embedded Studio 2.7.0/ARM/packs/AnalogDevices/ADuCM302x_DFP/3.1.0/Include/drivers/dma" -I"C:/Analog Devices/CrossCore Embedded Studio 2.7.0/ARM/packs/AnalogDevices/ADuCM302x_DFP/3.1.0/Include/drivers/pwr" -I"C:/Analog Devices/CrossCore Embedded Studio 2.7.0/ARM/packs/AnalogDevices/ADuCM302x_DFP/3.1.0/Include/drivers/tmr" -I"C:/Analog Devices/CrossCore Embedded Studio 2.7.0/ARM/packs/AnalogDevices/ADuCM302x_DFP/3.1.0/Include/drivers/uart" -I"C:/Analog Devices/CrossCore Embedded Studio 2.7.0/ARM/packs/AnalogDevices/ADuCM302x_DFP/3.1.0/Include/drivers/wdt" -I"C:\Users\ADrimbar\git\EVAL-ADICUP3029\projects\ADuCM3029_demo_cli/RTE" -I"C:\Users\ADrimbar\git\EVAL-ADICUP3029\projects\ADuCM3029_demo_cli/RTE/Device/ADuCM3029" -mcpu=cortex-m3 -mthumb -o "$@" "$<"
+	arm-none-eabi-gcc -c -x assembler-with-cpp -g -gdwarf-2 -DCORE0 -D_DEBUG -D_RTE_ -D__ADUCM3029__ -D__SILICON_REVISION__=0x100 @includes-f79955d40ecffa159853b6852bf087a8.txt -mcpu=cortex-m3 -mthumb -o  "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
 RTE/Device/ADuCM3029/%.o: ../RTE/Device/ADuCM3029/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: CrossCore GCC ARM Embedded C Compiler'
-	arm-none-eabi-gcc -g -gdwarf-2 -ffunction-sections -fdata-sections -DCORE0 -D_DEBUG -D_RTE_ -D__ADUCM3029__ -D__SILICON_REVISION__=0x100 @includes-ace88a7f676e681455857b8b214a14d7.txt -Wall -c -mcpu=cortex-m3 -mthumb -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o  "$@" "$<"
+	arm-none-eabi-gcc -g -gdwarf-2 -ffunction-sections -fdata-sections -DCORE0 -D_DEBUG -D_RTE_ -D__ADUCM3029__ -D__SILICON_REVISION__=0x100 @includes-afa06b5f1ce396f0c89a226ccea14b75.txt -Wall -c -mcpu=cortex-m3 -mthumb -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o  "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
--- projects/ADuCM3029_demo_cli/Debug/system/pinmux/GeneratedSources/subdir.mk
+++ projects/ADuCM3029_demo_cli/Debug/system/pinmux/GeneratedSources/subdir.mk
@@ -17,7 +17,7 @@ C_DEPS += \
 system/pinmux/GeneratedSources/%.o: ../system/pinmux/GeneratedSources/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: CrossCore GCC ARM Embedded C Compiler'
-	arm-none-eabi-gcc -g -gdwarf-2 -ffunction-sections -fdata-sections -DCORE0 -D_DEBUG -D_RTE_ -D__ADUCM3029__ -D__SILICON_REVISION__=0x100 @includes-ace88a7f676e681455857b8b214a14d7.txt -Wall -c -mcpu=cortex-m3 -mthumb -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o  "$@" "$<"
+	arm-none-eabi-gcc -g -gdwarf-2 -ffunction-sections -fdata-sections -DCORE0 -D_DEBUG -D_RTE_ -D__ADUCM3029__ -D__SILICON_REVISION__=0x100 @includes-afa06b5f1ce396f0c89a226ccea14b75.txt -Wall -c -mcpu=cortex-m3 -mthumb -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o  "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
