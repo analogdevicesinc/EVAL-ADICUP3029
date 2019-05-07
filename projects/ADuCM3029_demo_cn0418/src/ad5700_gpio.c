83d098e1079935123a7927fb1e33915c276304de
Andrei Drimbarean <Andrei.Drimbarean@analog.com>
2019-05-07 16:10:33 +0300

    cn0418: add component drivers
    Add drivers used in the implementation of this application that were
    used in the implementation of the sister project CN0414. The following
    list contains all the functionalities covered in this commit:
     - power management;
     - delay and timers;
     - I2C;
     - SPI;
     - GPIO;
     - UART
     - software UART;
     - 24LC32A EEPROM memory;
     - AD5700 HART modem implementation.
    
    These files were reviewd in the past with the following commits and
    should require minimal to no review this time:
    
    I2C, SPI, GPIO and UART (platform driver) commit:
    1d7d53978decf0f5eb12c54e003d806550010506
    
    Power and timer implementation commit:
    93afe5e6d56f97beb5cd8e86126ce5a7137d851d
    
    Software UART implementation:
    bd131d4d32c39d42c7c24da897d28b4b5b8c3212
    
    24LC32A EEPROM driver:
    8f073a2d703271fc6400b8a3cd6e89886fa5e0b9
    
    AD5700 HART modem driver:
    9c758cedf27fbd2efd52256e19af6574089e57e1
    
    Signed-off-by: Andrei Drimbarean <Andrei.Drimbarean@analog.com>

---
+++ projects/ADuCM3029_demo_cn0418/src/ad5700_gpio.c
@@ -0,0 +1,111 @@
+/***************************************************************************//**
+*   @file   ad5700_gpio.c
+*   @brief  ad5700 GPIO Abstraction Level source.
+*   @author Andrei Drimbarean (andrei.drimbarean@analog.com)
+********************************************************************************
+* Copyright 2018(c) Analog Devices, Inc.
+*
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*  - Redistributions of source code must retain the above copyright
+*    notice, this list of conditions and the following disclaimer.
+*  - Redistributions in binary form must reproduce the above copyright
+*    notice, this list of conditions and the following disclaimer in
+*    the documentation and/or other materials provided with the
+*    distribution.
+*  - Neither the name of Analog Devices, Inc. nor the names of its
+*    contributors may be used to endorse or promote products derived
+*    from this software without specific prior written permission.
+*  - The use of this software may or may not infringe the patent rights
+*    of one or more patent holders.  This license does not release you
+*    from the requirement that you obtain separate licenses from these
+*    patent holders to use this software.
+*  - Use of the software either in source or binary form, must be run
+*    on or directly connected to an Analog Devices Inc. component.
+*
+* THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR IMPLIED
+* WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT, MERCHANTABILITY
+* AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+* IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
+* INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+* LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
+* SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+* CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+
+/******************************************************************************/
+/***************************** Include Files **********************************/
+/******************************************************************************/
+#include <stdlib.h>
+#include "ad5700_gpio.h"
+
+/******************************************************************************/
+/************************* Functions Definitions ******************************/
+/******************************************************************************/
+
+/**
+ * Sets up callback function for Group B GPIO interrupts. The CD pin will be
+ * connected to this interrupt to enable HART reads on interrupt instead of
+ * calling the function when input is expected. This method functions as an
+ * abstraction wrapper to avoid including ADICUP3029 drivers in higher modules.
+ *
+ * @param [in] dev     	   - AD5700 device structure.
+ * @param [in] pf_callback - The function pointer to the callback function.
+ *
+ * @return 0 for success or negative error code.
+ */
+int32_t ad5700_register_gpio_callback(struct ad5700_dev *dev,
+				      ADI_CALLBACK const pf_callback)
+{
+	return adi_gpio_RegisterCallback(ADI_GPIO_INTB_IRQ, pf_callback, NULL);
+}
+
+/**
+ * Set up Group B GPIO interrupt for CD pin of the AD5700 and set the interrupt
+ * polarity so that the interrupt is triggered on a positive edge.
+ *
+ * @param [in] dev - AD5700 device structure.
+ *
+ * @return 0 for success or negative error code.
+ */
+int32_t ad5700_set_cd_interrupt(struct ad5700_dev *dev)
+{
+	int32_t ret;
+	uint8_t cd_port;
+	uint16_t cd_pin;
+	uint16_t gpio_register_data;
+
+	cd_pin = (ADI_GPIO_DATA)(0x0001 << (dev->gpio_cd->number & 0x0F));
+	cd_port = (dev->gpio_cd->number & 0xF0) >> 4;
+
+	ret = adi_gpio_GetGroupInterruptPins(cd_port,
+					     ADI_GPIO_INTB_IRQ,
+					     &gpio_register_data);
+	if(ret != 0)
+		return ret;
+
+	/* Add CD pin to the group B interrupts */
+	gpio_register_data |= cd_pin;
+
+	/* Set GPIO for interrupt */
+	ret = adi_gpio_SetGroupInterruptPins(cd_port,
+					     ADI_GPIO_INTB_IRQ,
+					     gpio_register_data);
+	if(ret != 0)
+		return ret;
+
+	/* Get group B interrupt polarity */
+	ret = adi_gpio_GetGroupInterruptPolarity(cd_port, &gpio_register_data);
+	if(ret != 0)
+		return ret;
+
+	/* Add CD pin to interrupt on a rising edge */
+	gpio_register_data |= cd_pin;
+
+	/* Set CD interrupt polarity */
+	return adi_gpio_SetGroupInterruptPolarity(cd_port, gpio_register_data);
+}
