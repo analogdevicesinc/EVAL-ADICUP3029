b3e8be018dda17324110a48f1df8f30b0a7a82b9
Andrei Drimbarean <Andrei.Drimbarean@analog.com>
2019-07-17 13:37:55 +0300

    cn0435: add license and comments
    Add licese notice to the ADuCM3029_demo_plcsystem.c main module and add
    some explicative comments. No functional changes.
    
    Signed-off-by: Andrei Drimbarean <Andrei.Drimbarean@analog.com>

--- projects/ADuCM3029_demo_cn0435/src/ADuCM3029_demo_plcsystem.c
+++ projects/ADuCM3029_demo_cn0435/src/ADuCM3029_demo_plcsystem.c
@@ -1,18 +1,72 @@
-/*****************************************************************************
- * ADuCM3029_demo_plcsystem.c
- *****************************************************************************/
+/***************************************************************************//**
+*   @file   ADuCM3029_demo_plcsystem.c
+*   @brief  Main module of the application; contains main() function.
+*   @author Andrei Drimbarean (andrei.drimbarean@analog.com)
+********************************************************************************
+* Copyright 2019(c) Analog Devices, Inc.
+*
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*  - Redistributions of source code must retain the above copyright
+*    notice, this list of conditions and the following disclaimer.
+*  - Redistributions in binary form must reproduce the above copyright
+*    notice, this list of conditions and the following disclaimer in
+*    the documentation and/or other materials provided with the
+*    distribution.
+*  - Neither the name of Analog Devices, Inc. nor the names of its
+*    contributors may be used to endorse or promote products derived
+*    from this software without specific prior written permission.
+*  - The use of this software may or may not infringe the patent rights
+*    of one or more patent holders.  This license does not release you
+*    from the requirement that you obtain separate licenses from these
+*    patent holders to use this software.
+*  - Use of the software either in source or binary form, must be run
+*    on or directly connected to an Analog Devices Inc. component.
+*
+* THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR IMPLIED
+* WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT, MERCHANTABILITY
+* AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+* IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
+* INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+* LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
+* SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+* CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+
+/******************************************************************************/
+/***************************** Include Files **********************************/
+/******************************************************************************/
 
 #include <sys/platform.h>
 #include "adi_initialize.h"
 #include "system_manager.h"
 #include "ad4111_regs.h"
 
+/******************************************************************************/
+/********************** Macros and Constants Definitions **********************/
+/******************************************************************************/
+
 /* Baudrate for software UART used for HART communication */
 #define HART_BAUDRATE 1200
+
+/******************************************************************************/
+/************************** Variable Definitions ******************************/
+/******************************************************************************/
 
 uint32_t unique_id = 0x00112233;
 float vref = 2.5;
 
+/******************************************************************************/
+/************************ Functions Definitions *******************************/
+/******************************************************************************/
+
+/**
+ * Main function of the application.
+ */
 int main(int argc, char *argv[])
 {
 	/**
