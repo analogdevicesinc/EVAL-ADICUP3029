0347bec070451f5786740e076210ad0262972a7c
Mihail Chindris <mihail.chindris@analog.com>
2020-03-30 13:18:40 +0300

    Add empty ADuCM3029_no_os_test_project
    Signed-off-by: Mihail Chindris <mihail.chindris@analog.com>

---
+++ projects/ADuCM3029_no_os_test_project/src/ADuCM3029_no_os_test_project.c
@@ -0,0 +1,22 @@
+/*****************************************************************************
+ * ADuCM3029_no_os_test_project.c
+ *****************************************************************************/
+
+#include <sys/platform.h>
+#include "adi_initialize.h"
+#include "ADuCM3029_no_os_test_project.h"
+
+int main(int argc, char *argv[])
+{
+	/**
+	 * Initialize managed drivers and/or services that have been added to 
+	 * the project.
+	 * @return zero on success 
+	 */
+	adi_initComponents();
+	
+	/* Begin adding your custom code here */
+
+	return 0;
+}
+
