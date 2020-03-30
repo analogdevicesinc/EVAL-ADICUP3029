0d9205136e309fedccad7f79cde632d311914bd6
Mihail Chindris <mihail.chindris@analog.com>
2020-03-30 13:18:40 +0300

    Add empty ADuCM3029_no_os_test_project
    Signed-off-by: Mihail Chindris <mihail.chindris@analog.com>

---
+++ projects/ADuCM3029_no_os_test_project/src/ADuCM3029_no_os_test_project.h
@@ -0,0 +1,10 @@
+/*****************************************************************************
+ * ADuCM3029_no_os_test_project.h
+ *****************************************************************************/
+
+#ifndef __ADUCM3029_NO_OS_TEST_PROJECT_H__
+#define __ADUCM3029_NO_OS_TEST_PROJECT_H__
+
+/* Add your custom header content here */
+
+#endif /* __ADUCM3029_NO_OS_TEST_PROJECT_H__ */
