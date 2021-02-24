ad6b613abf1d491dbefc1aad54fe7549ec019e72
Mihail Chindris <mihail.chindris@analog.com>
2021-02-24 09:31:42 -0500

    generic_variables.mk: Small fixes
    HARDWARE needs to be set here
    
    Signed-off-by: Mihail Chindris <mihail.chindris@analog.com>

--- generic_variables.mk
+++ generic_variables.mk
@@ -1,19 +1,22 @@
 PLATFORM = aducm3029
 
-PROJECT			?= $(realpath .)
-TARGET 			?= $(notdir $(realpath .))
-NO-OS			?= $(realpath ../../no-OS)
-WORKSPACE		?= $(realpath ..)
-
-INCLUDE			?= $(NO-OS)/include
-DRIVERS 		?= $(NO-OS)/drivers
-PLATFORM_DRIVERS	?= $(NO-OS)/drivers/platform/$(PLATFORM)
-
 ifeq '$(LOCAL_BUILD)' 'y'
 
 include ../../local_variables.mk
 
 endif
+
+PROJECT			?= $(realpath .)
+TARGET 			?= $(notdir $(realpath .))
+NO-OS			?= $(realpath ../../no-OS)
+BUILD_DIR_NAME		?= build
+WORKSPACE		?= $(PROJECT)/$(BUILD_DIR_NAME)
+
+INCLUDE			?= $(NO-OS)/include
+DRIVERS 		?= $(NO-OS)/drivers
+PLATFORM_DRIVERS	?= $(NO-OS)/drivers/platform/$(PLATFORM)
+
+HARDWARE ?= $(wildcard pinmux_config.c)
 
 # By default copy noos sources into the build directory of the project.
 LINK_SRCS ?= n
