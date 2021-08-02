348e663d9ab8376cb5374918572bb9a109a83266
Antoniu Miclaus <antoniu.miclaus@analog.com>
2021-08-02 13:50:01 +0300

    cn0503: update auto-generated CCES config files
    Update project specific auto-generated configuration files.
    
    Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

--- projects/ADuCM3029_demo_cn0503/system/pinmux/GeneratedSources/pinmux_config.c
+++ projects/ADuCM3029_demo_cn0503/system/pinmux/GeneratedSources/pinmux_config.c
@@ -1,8 +1,8 @@
 /*
  **
- ** Source file generated on October 15, 2019 at 16:08:22.	
+ ** Source file generated on August 2, 2021 at 11:30:30.	
  **
- ** Copyright (C) 2011-2019 Analog Devices Inc., All Rights Reserved.
+ ** Copyright (C) 2011-2021 Analog Devices Inc., All Rights Reserved.
  **
  ** This file is generated automatically based upon the options selected in 
  ** the Pin Multiplexing configuration editor. Changes to the Pin Multiplexing
@@ -11,17 +11,22 @@
  **
  ** Selected Peripherals
  ** --------------------
+ ** SPI0 (SCLK, MOSI, MISO, CS_1)
  ** I2C0 (SCL0, SDA0)
  ** UART0 (Tx, Rx)
  **
  ** GPIO (unavailable)
  ** ------------------
- ** P0_04, P0_05, P0_10, P0_11
+ ** P0_00, P0_01, P0_02, P0_04, P0_05, P0_10, P0_11, P1_10
  */
 
 #include <sys/platform.h>
 #include <stdint.h>
 
+#define SPI0_SCLK_PORTP0_MUX  ((uint16_t) ((uint16_t) 1<<0))
+#define SPI0_MOSI_PORTP0_MUX  ((uint16_t) ((uint16_t) 1<<2))
+#define SPI0_MISO_PORTP0_MUX  ((uint16_t) ((uint16_t) 1<<4))
+#define SPI0_CS_1_PORTP1_MUX  ((uint32_t) ((uint32_t) 1<<20))
 #define I2C0_SCL0_PORTP0_MUX  ((uint16_t) ((uint16_t) 1<<8))
 #define I2C0_SDA0_PORTP0_MUX  ((uint16_t) ((uint16_t) 1<<10))
 #define UART0_TX_PORTP0_MUX  ((uint32_t) ((uint32_t) 1<<20))
@@ -34,8 +39,10 @@ int32_t adi_initpinmux(void);
  */
 int32_t adi_initpinmux(void) {
     /* PORTx_MUX registers */
-    *((volatile uint32_t *)REG_GPIO0_CFG) = I2C0_SCL0_PORTP0_MUX | I2C0_SDA0_PORTP0_MUX
+    *((volatile uint32_t *)REG_GPIO0_CFG) = SPI0_SCLK_PORTP0_MUX | SPI0_MOSI_PORTP0_MUX
+     | SPI0_MISO_PORTP0_MUX | I2C0_SCL0_PORTP0_MUX | I2C0_SDA0_PORTP0_MUX
      | UART0_TX_PORTP0_MUX | UART0_RX_PORTP0_MUX;
+    *((volatile uint32_t *)REG_GPIO1_CFG) = SPI0_CS_1_PORTP1_MUX;
 
     return 0;
 }
